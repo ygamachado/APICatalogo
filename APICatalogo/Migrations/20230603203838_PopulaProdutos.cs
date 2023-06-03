0756286ba52fe578cc7ce61f26a461fea2b3f1e8
ygamachado <ygaelm@gmail.com>
2023-06-03 18:00:11 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Migrations/20230603203838_PopulaProdutos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+                " VALUES ('Coca-cola', 'refri', '8', 'coca.jpg', '10', now(), '1')");
+            mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+                " VALUES ('Lanchinho', 'haburgão', '5', 'lanchinho.jpg', '14', now(), '2')");
+            mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+                " VALUES ('Pudim', 'bom demaisi', '4', 'pudim.jpg', '40', now(), '3')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
